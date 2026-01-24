326f8dc5fe898ab4d1a5d2842498dc727c26ecd1
hesonthemove-ux <hesonthemove@gmail.com>
2026-01-24 11:42:39 +0000

    Update sw.js
    adjust background image names to correct

--- sw.js
+++ sw.js
@@ -7,13 +7,13 @@ const ASSETS = [
   './assets/branding/logo.png',
   './assets/crew/dj_alex.jpg',
   './assets/crew/dj_mhairi.jpg',
-  './assets/background/background1.jpg',
-  './assets/background/background2.jpg',
-  './assets/background/background3.jpg',
-  './assets/background/background4.jpg',
-  './assets/background/background5.jpg',
-  './assets/background/background6.jpg',
-  './assets/background/background7.jpg'
+  './assets/background/bg1.jpg',
+  './assets/background/bg22.jpg',
+  './assets/background/bg3.jpg',
+  './assets/background/bg4.jpg',
+  './assets/background/bg5.jpg',
+  './assets/background/bg6.jpg',
+  './assets/background/bg7.jpg'
 ];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
